29353c638189168d2e2753d015c96e02ac60af41
Huy Tran <tanhuy998@gmailc.om>
2019-04-23 23:56:49 +0700

    modify .htaccess for allowing access public contents

--- src/autoload/autoload_map.php
+++ src/autoload/autoload_map.php
@@ -4,7 +4,7 @@
     $base_path = dirname(dirname(__DIR__));
     
     return [
-        'TestController' => $base_path.'\public\controller\TestController.php',
+        'TestController' => $base_path.'\MVC\controller\TestController.php',
         'Router' => $base_path.'\src\router\router.php',
         'RouteMap' => $base_path.'\src\router\routemap.php',
         'Route' => $base_path.'\src\router\route.php',
